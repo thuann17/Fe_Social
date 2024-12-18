0aea79f11d8b9d3b86d2039741c769de8f48b470
thuann17 <117891819+thuann17@users.noreply.github.com>
2024-12-18 12:19:10 +0700

    app

--- src/App.js
+++ src/App.js
@@ -1,35 +1,52 @@
 import "./App.css";
 import React from "react";
+import { HTML5Backend } from "react-dnd-html5-backend";
+import { DndProvider } from "react-dnd";
 import {
   Route,
   RouterProvider,
   createBrowserRouter,
   createRoutesFromElements,
-} from "react-router-dom";
+} from "react-router-dom"; // Đảm bảo là từ react-router-dom
 
+// Các Layouts và Pages
 import UserLayout from "./Layouts/UserLayout";
 import AdminLayout from "./Layouts/AdminLayout";
-
+import TripPlanner from "./Pages/user/Trip/TripPlanner";
+import TripStart from "./Pages/user/Trip/TripStart";
+import Login from "./Pages/login/login";
+import ForgotPassword from "./Pages/login/ForgotPassword";
+import Chat from './Pages/user/Chat/Chat'
 function App() {
   const route = createBrowserRouter(
     createRoutesFromElements(
       <>
+
         {/* Định tuyến User */}
-        <Route path="/user">
-          <Route path="index" element={<UserLayout />} />
+        <Route path="/user" element={<UserLayout />} >
+          <Route path="index" />
+          <Route path="cal" element={<TripPlanner />} />
+          <Route path="start" element={<TripStart />} />
         </Route>
 
         {/* Định tuyến Admin */}
-        <Route path="/admin" element={<AdminLayout />}>
-          {/* <Route path="dashboard" element={<AdminDashboard />} /> */}
+        <Route path="/admin">
+          <Route path="dashboard" element={<AdminLayout />} />
         </Route>
+
+        {/* Định tuyến cho Login và ForgotPassword */}
+        <Route path="/login" element={<Login />} />
+        <Route path="/forgot" element={<ForgotPassword />} />
+        <Route path="/chat" element={<Chat />} />
       </>
     )
   );
 
   return (
     <React.StrictMode>
-      <RouterProvider router={route} />
+      <DndProvider backend={HTML5Backend}>
+        <RouterProvider router={route} />
+      </DndProvider>
     </React.StrictMode>
   );
 }
--- src/Layouts/UserLayout.js
+++ src/Layouts/UserLayout.js
@@ -1,7 +1,8 @@
 import React from "react";
 import Header from "../Components/user/header";
 import RightSidebar from "../Components/user/rightsidebar";
-import PostList from "../Pages/user/Post/PostList";
+import TripPlanner from "../Pages/user/Trip/TripPlanner";
+import { Outlet } from "react-router-dom";
 const UserLayout = () => {
   return (
     <div className="bg-gray-100 min-h-screen">
@@ -10,13 +11,12 @@ const UserLayout = () => {
 
       {/* Main Content */}
       <div className="flex justify-center px-4 py-4">
-        {/* Post List (Center) */}
         <div className="w-full max-w-3xl"> {/* Giới hạn chiều rộng của khu vực bài viết */}
-          <PostList />
+        <Outlet />
         </div>
 
         {/* Right Sidebar */}
-        <RightSidebar />
+        {/* <RightSidebar /> */}
       </div>
     </div>
   );
